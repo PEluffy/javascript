7aa6e1deea40326674c11670b1f0a7bd8aad9a8d
Prashannakafle4444 <kafleprashanna4444@gmail.com>
2024-02-20 16:18:55 +0545

    weather app and using local storage

--- weather/script.js
+++ weather/script.js
@@ -2,6 +2,7 @@ const button = document.getElementById("search-button");
 const cityInput = document.getElementById("city-input");
 const locationtext = document.getElementById("location");
 const weatherText = document.getElementById("weather");
+const previous = document.getElementById("pre");
 async function getData(cityName) {
   const promise = await fetch(
     `http://api.weatherapi.com/v1/current.json?key=acddaa3e2bf9492183780642241902&q=${cityName}&aqi=yes`
@@ -15,10 +16,17 @@ const apiKey = button.addEventListener("click", async () => {
   const value = cityInput.value;
   const result = await getData(value);
   const temp = result.current.temp_c;
+  localStorage.setItem("location", result.location.country);
 
   locationtext.innerText = `${result.location.name}`;
   weatherText.innerText = `${temp} degree celcius ${
     isCold(temp) ? "🥶" : "🥵"
   }`;
   locationtext.style.color = "blue";
+  const lo = localStorage.getItem("location");
+  previous.innerText = ` Country ${lo}`;
 });
+// window.addEventListener("load", () => {
+//   const lo = localStorage.getItem("location");
+//   previous.innerText = ` Country ${lo}`;
+// });
