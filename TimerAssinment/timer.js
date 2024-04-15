b16e2cafd72eababff3dea70006d0c0a410c7301
Prashannakafle4444 <kafleprashanna4444@gmail.com>
2024-04-15 13:36:53 +0545

    Using  Teachable machine to make my model learn and using in the timer clock

--- TimerAssinment/timer.js
+++ TimerAssinment/timer.js
@@ -9,11 +9,71 @@ let interval;
 const button_reset = document.getElementById("reset");
 const button_start = document.getElementById("start");
 const button_stop = document.getElementById("stop");
+const button_predict = document.getElementById("startPrediction");
+const URL = "https://teachablemachine.withgoogle.com/models/xM1I7vN_G/";
+
+let model, webcam, labelContainer, maxPredictions;
 // let stop = true;
 // let start = false;
+async function init() {
+  const modelURL = URL + "model.json";
+  const metadataURL = URL + "metadata.json";
+
+  // load the model and metadata
+  // Refer to tmImage.loadFromFiles() in the API to support files from a file picker
+  // or files from your local hard drive
+  // Note: the pose library adds "tmImage" object to your window (window.tmImage)
+  model = await tmImage.load(modelURL, metadataURL);
+  maxPredictions = model.getTotalClasses();
+
+  // Convenience function to setup a webcam
+  const flip = true; // whether to flip the webcam
+  webcam = new tmImage.Webcam(200, 200, flip); // width, height, flip
+  await webcam.setup(); // request access to the webcam
+  await webcam.play();
+  window.requestAnimationFrame(loop);
+
+  // append elements to the DOM
+  // document.getElementById("webcam-container").appendChild(webcam.canvas); //mighht need
+  labelContainer = document.getElementById("label-container");
+  for (let i = 0; i < maxPredictions; i++) {
+    // and class labels
+    labelContainer.appendChild(document.createElement("div"));
+  }
+}
+async function loop() {
+  webcam.update(); // update the webcam frame
+  await predict();
+  window.requestAnimationFrame(loop);
+}
+async function predict() {
+  // predict can take in an image, video or canvas html element
+  const prediction = await model.predict(webcam.canvas);
+  for (let i = 0; i < maxPredictions; i++) {
+    const classPrediction =
+      prediction[i].className + ": " + prediction[i].probability.toFixed(2);
+    labelContainer.childNodes[i].innerHTML = classPrediction;
+    const arrayPrediction = [...prediction];
+    const highestPredicted = arrayPrediction.reduce(function (acc, curr) {
+      return acc.probability > curr.probability ? acc : curr;
+    }).className;
+    handleClick(highestPredicted);
+  }
+}
+function handleClick(idName) {
+  console.log(idName);
+  const button = document.getElementById(`${idName}`);
+  console.log(button);
+  if (button) {
+    button.click();
+  }
+}
+button_predict.addEventListener("click", init);
+
 button_start.addEventListener("click", () => {
   //   stop = false;
   //   start = true;
+
   button_start.disabled = true;
   button_stop.disabled = false;
   button_reset.disabled = false;
